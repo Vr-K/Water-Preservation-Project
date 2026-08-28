6a86415c2435092f2bb14aefec894212ce1c3826
Veera K <korkeamaki.veera@gmail.com>
2026-08-28 22:46:53 +0300

    Update train.py
    updated info on epochs

--- files/train.py
+++ files/train.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     results = model.train(
         data='LandscapeSegmentation.v3i.yolov8/data.yaml',                   # get data.yaml file, i think you can also get just use the dataset folder location here
         imgsz=768,                                                           # can be ignored, otherwise resizes the image size
-        epochs=50,                                                           # epoch is how many completed runs
+        epochs=50,                                                           # epoch is how many completed runs 50 or 100 epoch is recommended. either or more is fine, model will save the best.pt anyway
         batch=8,                                                             # batch is how many files per segction
         name='yolo8m_seg_custom'                                             # name of the given model
     )
